4a2bc3df6c4236ede0285df4f4cc58189018156a
QuarkInComputing <jackmcleanwilson@gmail.com>
2025-01-01 15:21:22 +0000

    Added user speed control
    Caps on speed included to avoid program breaking or "reversing" at speeds below 0

--- game/main.lua
+++ game/main.lua
@@ -2,18 +2,30 @@ function love.load()
 
 	love.window.setMode(600, 600)
 
+	-- Position
 	x = 100
 	y = 50
-	width = 200
-	height = 150
+
+	-- Velocity
 	dx = 40 
 	dy = 60
+
+	-- Dimensions
+	width = 200
+	height = 150
+
+	-- Multiplier
+	speedMult = 1
+	
 end
 
 function love.update(dt)
-	x = x + dx * dt
-	y = y + dy * dt
 
+	-- Position Update
+	x = x  + (dx * dt * speedMult)
+	y = y + (dy * dt * speedMult)
+
+	-- Rectangle Movement
 	if (x > 600 - width) or (x < 0) then
 		dx = -dx
 		print("New dx: " .. tostring(dx))
@@ -21,6 +33,15 @@ function love.update(dt)
 		if (y > 600 - height) or (y < 0) then
 		print("New dy: " .. tostring(dy))
 		dy = -dy
+	end
+
+	-- User Input
+	if love.keyboard.isDown("up") and speedMult < 8 then
+		speedMult = speedMult + 0.01
+		print("New mult: " .. tostring(speedMult))
+	elseif love.keyboard.isDown("down") and speedMult > 0.5 then 
+		speedMult = speedMult - 0.01	
+		print("New mult: " .. tostring(speedMult))
 	end
 end
 
