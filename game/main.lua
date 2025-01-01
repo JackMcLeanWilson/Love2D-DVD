d2fa46eed4871289892e9a79b7f1ff70bff6c0a3
QuarkInComputing <jackmcleanwilson@gmail.com>
2025-01-01 14:31:16 +0000

    Cube bounce on wall

--- game/main.lua
+++ game/main.lua
@@ -1,15 +1,29 @@
 function love.load()
+
+	love.window.setMode(600, 600)
+
 	x = 100
 	y = 50
+	width = 200
+	height = 150
 	dx = 40 
 	dy = 60
 end
 
 function love.update(dt)
-	x = x + dx * dt
-	y = y + dx * dt
+	x = (x + dx) * dt
+	y = (y + dx) * dt
+
+	if x > 600 - width then
+		print("Out of X bounds!")
+		dx = -dx
+	end
+		if y > 600 - height then
+		print("Out of Y bounds!")
+		dy = -dy
+	end
 end
 
 function love.draw()
-	love.graphics.rectangle("fill", x, y, 200, 150)
+	love.graphics.rectangle("fill", x, y, width, height)
 end
