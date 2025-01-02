c48c39661460c4721f102825dfc70f0950a0b2d2
QuarkInComputing <jackmcleanwilson@gmail.com>
2025-01-02 19:53:43 +0000

    Added Menu

--- game/main.lua
+++ game/main.lua
@@ -9,6 +9,7 @@ function love.load()
 	love.graphics.setFont(Font)
 
 	DisplayText = ""
+	GameState = "menu"
 
 	--Sprites 
 	DVDSprite = love.graphics.newImage("assets/DVDlogo/LogoW.png")
@@ -40,56 +41,81 @@ end
 
 function love.update(dt)
 
-	-- Position Update
-	Xpos = Xpos  + (DX * dt * SpeedMult)
-	Ypos = Ypos + (DY * dt * SpeedMult)
+	if GameState == "menu" then
+		DisplayText = "Endless DVD"
+		ControlText = "Up Arrow - Speed Up\nDown Arrow - Speed Down\nR - Reset Speed\nQ- Reset\nEsc - Exit"
+		DisplayText2 = "Press Space To Start!"
+		if love.keyboard.isDown("space") then
+			GameState = "game"
+			DisplayText = ""
+		end
+		if love.keyboard.isDown("escape") then
+			love.event.quit()
+		end
 
-	-- Rectangle Bounds Check
-	if (Xpos > 600 - (Width + 3)) or (Xpos < 3) then
-		DX = -DX
-		print("New DX: " .. tostring(DX))
-		RandomColor()
-		love.graphics.setColor(SpriteColor)
-		SoundHit:play()
-	end
-	if (Ypos > 600 - (Height + 3)) or (Ypos < 3) then
-		DY = -DY
-		print("New DY: " .. tostring(DY))
-		RandomColor()
-		love.graphics.setColor(SpriteColor)
-		SoundHit:play()
-	end
+	elseif GameState == "game" then
+		-- Position Update
+		Xpos = Xpos  + (DX * dt * SpeedMult)
+		Ypos = Ypos + (DY * dt * SpeedMult)
 
-	-- User Input
-	if love.keyboard.isDown("up") and SpeedMult ~= 4 then --Speed Up
-		SpeedMult = 4
-		print("New mult: " .. tostring(SpeedMult))
-		DisplayText = "Fast Speed!"
-		SoundSpeed:play()
-	elseif love.keyboard.isDown("down") and SpeedMult ~= 1 then  -- Speed Down
-		SpeedMult = 1
-		print("New mult: " .. tostring(SpeedMult))
-		DisplayText = "Slow Speed!"
-		SoundSpeed:play()
-	elseif love.keyboard.isDown("r") and SpeedMult ~= 2 then -- Reset Speed 
-		SpeedMult = 2
-		print("Mult Reset")
-		DisplayText = "Normal Speed!"
-		SoundSpeed:play()
-	elseif love.keyboard.isDown("q") then  --Reset Position
-		Xpos = 100
-		Ypos = 100
-		SoundBlip:play()
+		-- Rectangle Bounds Check
+		if (Xpos > 600 - (Width + 5)) or (Xpos < 3) then
+			DX = -DX
+			print("New DX: " .. tostring(DX))
+			RandomColor()
+			love.graphics.setColor(SpriteColor)
+			SoundHit:play()
+		end
+		if (Ypos > 600 - (Height + 5)) or (Ypos < 3) then
+			DY = -DY
+			print("New DY: " .. tostring(DY))
+			RandomColor()
+			love.graphics.setColor(SpriteColor)
+			SoundHit:play()
+		end
+
+		-- User Input
+		if love.keyboard.isDown("up") and SpeedMult ~= 4 then --Speed Up
+			SpeedMult = 4
+			print("New mult: " .. tostring(SpeedMult))
+			DisplayText = "Fast Speed!"
+			SoundSpeed:play()
+		elseif love.keyboard.isDown("down") and SpeedMult ~= 1 then  -- Speed Down
+			SpeedMult = 1
+			print("New mult: " .. tostring(SpeedMult))
+			DisplayText = "Slow Speed!"
+			SoundSpeed:play()
+		elseif love.keyboard.isDown("r") and SpeedMult ~= 2 then -- Reset Speed 
+			SpeedMult = 2
+			print("Mult Reset")
+			DisplayText = "Normal Speed!"
+			SoundSpeed:play()
+		elseif love.keyboard.isDown("q") then  --Reset Position
+			Xpos = 100
+			Ypos = 100
+			SoundBlip:play()
+		elseif love.keyboard.isDown("escape") then
+			love.event.quit()
+		end
 	end
+	
 end
 
 function love.draw()
-	love.graphics.setColor(SpriteColor)
-	love.graphics.draw(DVDSprite, Xpos, Ypos, 0, Width / DVDSprite:getWidth(), Height / DVDSprite:getHeight())
 
-	if DisplayText ~= "" then
+	if GameState == "menu" then
 		love.graphics.printf(DisplayText, 0, 50, 600, "center")
+		love.graphics.printf(ControlText, 0, 150, 600, "center")
+		love.graphics.printf(DisplayText2, 0, 300, 600, "center")
+	elseif GameState == "game" then
+		love.graphics.setColor(SpriteColor)
+		love.graphics.draw(DVDSprite, Xpos, Ypos, 0, Width / DVDSprite:getWidth(), Height / DVDSprite:getHeight())
+
+		if DisplayText ~= "" then
+			love.graphics.printf(DisplayText, 0, 50, 600, "center")
+		end
 	end
+	
 end
 
 function RandomColor()
